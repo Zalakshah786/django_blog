15bcbf87db499c4d566f6898da1a5b3999496773
Zalak Shah <oel232ec@students.codeinstitute.net>
2025-01-17 15:06:26 +0000

    blog and posts url add in both url.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
-from . import views
+from .import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('posts/', views.PostList.as_view(), name='home'),
 ]
--- codestar/urls.py
+++ codestar/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
 
-urlpatterns = [
+
    
+urlpatterns = [
     path("blog/", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
 ]
