ede8651fc68d3f2c33bce2e990026253d229ef28
Zalak Shah <oel232ec@students.codeinstitute.net>
2025-01-19 00:18:26 +0000

    1 view topic done

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 from .import views
 from django.urls import path
 
+#urlpatterns = [
+    #path('', views.PostList.as_view(), name='home'),
+   # path('<slug:slug>/', views.post_detail, name='post_detail'),
+#]
+
+
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('', views.EventsList.as_view(), name='home'),
+    path("", views.event_detail, name="event_detail") 
 ]
