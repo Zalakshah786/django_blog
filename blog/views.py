b4d48a97d2ffa35b241f199d61416839321dfcdc
Zalak Shah <oel232ec@students.codeinstitute.net>
2025-01-20 20:22:08 +0000

    adding comment

--- blog/views.py
+++ blog/views.py
@@ -26,8 +26,17 @@ def post_detail(request, slug):
     """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-    return render(request, "blog/post_detail.html", {"post": post},)
-
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+    return render(
+    request,
+    "blog/post_detail.html",
+    {
+        "post": post,
+        "comments": comments,
+        "comment_count": comment_count,
+    },
+)
 class EventsList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
