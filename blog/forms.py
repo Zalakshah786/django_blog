5d5d6edebe0aec5c0e04f3768755365db1130707
Zalak Shah <oel232ec@students.codeinstitute.net>
2025-01-20 23:16:56 +0000

    last form things add

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,17 @@
-from .models import Comment
+from ..blog.models import Comment
+
+from ..blog.models import CollaborateRequest
 from django import forms
+
+
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+class CollaborateForm(forms.ModelForm):
+    class Meta:
+        model = CollaborateRequest
+        fields = ('name', 'email', 'message')
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Post
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
 from .forms import CommentForm
 
 # Create your views here.
@@ -31,8 +32,9 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
-      comment_form = CommentForm(data=request.POST)
-      if comment_form.is_valid():
+       print("Received a POST request")
+    comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid():
         comment = comment_form.save(commit=False)
         comment.author = request.user
         comment.post = post
@@ -42,6 +44,8 @@ def post_detail(request, slug):
             'Comment submitted and awaiting approval'
     )
     comment_form = CommentForm()
+    print("About to render template")
+
     return render(
     request,
     "blog/post_detail.html",
